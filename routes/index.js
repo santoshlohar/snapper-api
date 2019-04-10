eccca3b4a189be3bfddf10697fb47dbd1fc0994d
Santosh Lohar <santosh.lohar@snapperfuturetech.com>
2019-04-10 12:32:17 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 
 router.use("/api/v1/user", require("../routes/v1/user/route"));
 router.use("/api/v1/institute", require("../routes/v1/institute/route"));
+router.use("/api/v1/mailer", require("../routes/v1/mailer/route"));
 
 module.exports = router;
